fba412b9760b8295b301539d882116a5e0cd21e5
Cristian Gavrila <cristian.gavrila@ametras.ro>
2024-07-23 10:33:34 +0300

    OpenAPI

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common'
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('orders')
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
